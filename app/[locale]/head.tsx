19973afc10b3d418dcffb1a7741eb0e4d719c14c
GitHub Copilot <git@github.com>
2026-02-09 23:40:12 +0100

    shinra

--- app/[locale]/head.tsx
+++ app/[locale]/head.tsx
@@ -1,14 +1,14 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" href="logo.png" />
+      <link rel="icon" href="/icon-192.png" />
       <link rel="manifest" href="/manifest.json" />
       <meta name="theme-color" content="#3b82f6" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="Schepen Kring" />
-      <link rel="apple-touch-icon" href="logo.png" />
+      <link rel="apple-touch-icon" href="/icon-192.png" />
       <link
         href="https://api.mapbox.com/mapbox-gl-js/v3.12.0/mapbox-gl.css"
         rel="stylesheet"
