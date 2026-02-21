a72ac3d5a7d20428621dafbcf8b9bfe9410f8ced
GitHub Copilot <git@github.com>
2026-02-21 18:00:56 +0100

    shinra

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -61,10 +61,6 @@ export default async function RootLayout({
           ${playfair.variable} 
           ${geistMono.variable} 
           antialiased 
-          bg-[#ffffff] 
-          text-black
-          selection:bg-[#c5a572]/30 
-          selection:text-[#7278c5]
         `}
       >
         <NextIntlClientProvider locale={locale}>
